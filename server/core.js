21443cc49a45e9416647cf9a8329df540e8eea8d
alexunwin <alex.unwin@infospace.com>
2015-11-07 00:52:29 -0800

    update core

--- server/core.js
+++ server/core.js
@@ -1,7 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    elasticsearch = require('elasticsearch'),
+    bodyParser = require('body-parser'),
+    story = require('./story');
 
 var app = express();
+
+var elasticsearchEndpoint = "52.32.221.20:9200";
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,13 +19,58 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.post('/portal', function (req, res) {
+// init elasticsearch
+var esClient = new elasticsearch.Client({
+        host: elasticsearchEndpoint
+});
+
+app.post('/story', function (req, res) {
+    if(!req.body) {
+        res.send('failure');
+        return;
+    }
+    story.create(esClient, req.body);
+    res.send('success');
+});
+
+app.get('/story', function(req, res) {
+    if(!Object.keys(req.query).length || req.query.id === undefined) {
+        res.send('failure');
+        return;
+    }
+
+    story.get(esClient, req.query.id, function(val) {
+        res.send(val);
+    });
+});
+
+app.post('/search/stories', function (req, res) {
     if(!req.body) {
         res.send('failure');
         return;
     }
 
-    res.end();
+    story.search(esClient, req.body, function(val){
+        res.send(val);
+    });
+});
+
+app.post('/search/user', function (req, res) {
+    if(!req.body) {
+        res.send('failure');
+        return;
+    }
+
+    res.send();
+});
+
+app.post('/search/lawyer', function (req, res) {
+    if(!req.body) {
+        res.send('failure');
+        return;
+    }
+
+    res.send();
 });
 
 var server = app.listen(3030, function () {
